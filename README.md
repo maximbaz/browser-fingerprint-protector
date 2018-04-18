56a89847ff8a86161a259c9a72268224f8357f86
Maxim Baz <git@maximbaz.com>
2018-04-18 13:45:41 +0200

    Implement `window.navigator` protection

---
+++ README.md
@@ -0,0 +1,13 @@
+# Browser Fingerprint Protector
+
+## How it works
+
+The extension protects the information in `window.navigator` and HTTP request headers to complicate browser fingerprinting.
+
+* Completely hides `mimeTypes` and `plugins`.
+* Pretends that you only have `English US` language.
+* Pretends that you use `Windows 10`.
+
+## Credits
+
+Icon made by [Freepik](http://www.freepik.com) from [Flaticon](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/).
---
+++ inject.js
@@ -0,0 +1,32 @@
+(function() {
+  "use strict";
+
+  const originalUserAgent = window.navigator.userAgent;
+  const fakeUserAgent = originalUserAgent.replace(
+    /\(.*?\)/,
+    "(Windows NT 10.0; Win64; x64)"
+  );
+  const fakeVersion = fakeUserAgent.substr(8);
+
+  window.navigator.__defineGetter__("appVersion", function() {
+    return fakeVersion;
+  });
+  window.navigator.__defineGetter__("language", function() {
+    return "en-US";
+  });
+  window.navigator.__defineGetter__("languages", function() {
+    return ["en-US", "en"];
+  });
+  window.navigator.__defineGetter__("mimeTypes", function() {
+    return { length: 0 };
+  });
+  window.navigator.__defineGetter__("platform", function() {
+    return "Win32";
+  });
+  window.navigator.__defineGetter__("plugins", function() {
+    return { length: 0 };
+  });
+  window.navigator.__defineGetter__("userAgent", function() {
+    return fakeUserAgent;
+  });
+})();
