f5ecad5134efc94b003f5011572fbffd1a4ae61d
Maxim Baz <git@maximbaz.com>
2018-04-18 14:17:37 +0200

    Implement HTTP request headers protection

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === "User-Agent") {
+        const originalUserAgent = details.requestHeaders[i].value;
+        const fakeUserAgent = originalUserAgent.replace(
+          /\(.*?\)/,
+          "(Windows NT 10.0; Win64; x64)"
+        );
+        details.requestHeaders[i].value = fakeUserAgent;
+      } else if (details.requestHeaders[i].name === "Accept-Language") {
+        details.requestHeaders[i].value = "en-US,en;q=0.5";
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking", "requestHeaders"]
+);
